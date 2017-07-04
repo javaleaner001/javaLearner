3003f1acd020d27dedffa37a5330e28ef27a94d8
xlf <fuxingli7313@126.com>
2017-07-04 16:08:21 +0800

    Idea test

--- src/main/java/com/xlf/learnSth/LearnSth.java
+++ src/main/java/com/xlf/learnSth/LearnSth.java
@@ -2,8 +2,9 @@ package com.xlf.learnSth;
 
 public class LearnSth {
 
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("idea test");
+    }
 
 }
